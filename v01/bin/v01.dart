0fc3d4e2da07ef7289b00fe0931e160d3922d663
Leo Browaldh <leobrowaldh@gmail.com>
2025-09-25 14:10:27 +0200

    created new dart project

---
+++ v01/bin/v01.dart
@@ -0,0 +1,5 @@
+import 'package:v01/v01.dart' as v01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${v01.calculate()}!');
+}
---
+++ v01/test/v01_test.dart
@@ -0,0 +1,8 @@
+import 'package:v01/v01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
