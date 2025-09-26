6e73805056197e8622beb2dce97ed43e436f2c25
Leo Browaldh <leobrowaldh@gmail.com>
2025-09-26 11:38:49 +0200

    calculator implemented

--- v01/bin/v01.dart
+++ v01/bin/v01.dart
@@ -1,5 +1,61 @@
-import 'package:v01/v01.dart' as v01;
+import 'dart:io';
+
+import 'package:v01/calculator.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${v01.calculate()}!');
+  print('Calculator: ');
+  final calc = Calculator();
+  while (true) {
+    print(
+      '\n'
+      'Enter an operation in the following format:\n'
+      '(1 + 3, 5 - 2, 5 * 2, 6 / 2)\n'
+      'or "exit" to quit:',
+    );
+    String? userInput = stdin.readLineSync();
+    if (userInput == null) {
+      print('Invalid input. Please try again.');
+      continue;
+    }
+    if (userInput.toLowerCase() == 'exit') {
+      print('Exiting calculator. Goodbye!');
+      break;
+    }
+    List<String> parts = userInput.split(' ');
+    if (parts.length != 3) {
+      print('Invalid input format. Please try again.');
+      continue;
+    }
+
+    double? a = double.tryParse(parts[0]);
+    String operator = parts[1];
+    double? b = double.tryParse(parts[2]);
+    if (a == null || b == null) {
+      print('Invalid numbers. Please try again.');
+      continue;
+    }
+    switch (operator) {
+      case '+':
+        print(calc.add(a, b));
+        break;
+      case '-':
+        print(calc.subtract(a, b));
+        break;
+      case '*':
+        print(calc.multiply(a, b));
+        break;
+      case '/':
+        try {
+          print(calc.divide(a, b));
+        } on ArgumentError catch (e) {
+          print(e.message);
+          break;
+        }
+        print(calc.divide(a, b));
+        break;
+      default:
+        print('Invalid operator. Please use +, -, *, or /.');
+        continue;
+    }
+  }
 }
